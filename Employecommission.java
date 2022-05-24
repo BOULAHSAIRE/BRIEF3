b9ecff9906a830a81d2339ce413f03a3cc82d995
BOULAHSAIRE <106145562+BOULAHSAIRE@users.noreply.github.com>
2022-05-24 10:21:58 +0200

    Add files via upload

---
+++ Employecommission.java
@@ -0,0 +1,36 @@
+package Brief2;
+
+public class Employecommission extends Employefixe {
+	
+
+		    private double CommissionTarif;
+		    //private double BaseSalaire;
+		    private double ventestotal;
+
+		    
+		    
+		      
+		    public Employecommission( String Prenom, String Nom, int Date, int salaire, double CommissionTarif, double Ventestotal) {
+		        super(Nom ,Prenom, Date, salaire );
+		        
+		        this.CommissionTarif = CommissionTarif;
+		       // this.BaseSalaire = Basesalaire;
+		        this.ventestotal = Ventestotal;
+		    }
+		        
+		       
+
+			 public double calculerSalaire()
+			    {
+			        return  super.getsalaire()+(ventestotal * CommissionTarif);
+			    }
+
+		
+			    @Override
+			    public String toString() {        
+			       return  super.toString() + 
+			    		   String.format( "Salaire:", calculerSalaire());
+			    }
+
+		}
+
---
+++ Main.java
@@ -0,0 +1,23 @@
+package Brief2;
+import java.util.Scanner;
+
+public class Main {
+			public static void main(String[] args) {
+				
+				Employefixe emp1 = new Employefixe("lilian","amam",23,4000);
+				System.out.println(emp1.toString());
+				// employe comission
+				 Employehoraire  emphor = new  Employehoraire ("amal", "boul",30, 60, 0.5);
+				 System.out.println(emphor.toString());
+				//employee horaire
+				 Employecommission empCm = new Employecommission("nawal", "boul", 25, 5000, 0.5, 60);
+				 System.out.println(empCm.toString());
+
+	
+	}
+	
+	
+	
+	
+
+}
