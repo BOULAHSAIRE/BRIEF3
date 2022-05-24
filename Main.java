c15385377f512bbdd1a429141d5ddb576ec96335
BOULAHSAIRE <106145562+BOULAHSAIRE@users.noreply.github.com>
2022-05-24 12:01:03 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Main {
 			public static void main(String[] args) {
 				
-				Employefixe emp1 = new Employefixe("lilian","amam",23,4000);
+				Employefixe emp1 = new Employefixe("Laila","Tamam",23,4000);
 				System.out.println(emp1.toString());
 				// employe comission
-				 Employehoraire  emphor = new  Employehoraire ("amal", "boul",30, 60, 0.5);
+				 Employehoraire  emphor = new  Employehoraire ("Amal", "Boulahsaire",30, 60, 0.5);
 				 System.out.println(emphor.toString());
 				//employee horaire
-				 Employecommission empCm = new Employecommission("nawal", "boul", 25, 5000, 0.5, 60);
+				 Employecommission empCm = new Employecommission("Imanr", "boul", 25, 5000, 0.5, 60);
 				 System.out.println(empCm.toString());
 
 	
